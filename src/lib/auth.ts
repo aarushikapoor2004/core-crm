5ef64df889364b83f0554d9caa4473d75cc2e869
Varun <devvarunbhardwaj@gmail.com>
2025-06-01 05:07:44 +0530

    added image in schema

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,41 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { db as prisma } from "@/db";
+import { PROTECTED_ROUTES } from "@/constants";
+
+
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  pages: { signIn: "/login" },
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        let existingUser = await prisma.user.findUnique({ where: { email: user.email! } });
+        if (!existingUser && user.email) {
+          existingUser = await prisma.user.create({
+            data: {
+              email: user.email,
+              imageUrl: user.image,
+              name: user.name ?? generateId(user.email),
+            },
+          });
+        }
+        if (existingUser) {
+          user.id = existingUser.id;
+        }
+
+        return true;
+      } catch (error) {
+        console.error("Error during sign-in:", error);
+        return false;
+      }
+    },
+  },
 });
