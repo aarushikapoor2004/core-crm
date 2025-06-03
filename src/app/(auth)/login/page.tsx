e4e6c50a35a26c384cc9c48f335ed8256799c5b8
aarushi kappoor <aarushik250@gmail.com>
2025-06-03 15:46:17 +0530

    added all

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,20 +1,25 @@
-import { GalleryVerticalEnd } from "lucide-react"
+"use client";
 
-import { LoginForm } from "@/components/login-form"
-import Link from "next/link"
+import { GalleryVerticalEnd } from "lucide-react";
+import { LoginForm } from "@/components/login-form";
+import Link from "next/link";
+import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6 -mt-50">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6 -mt-12">
         <Link href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-            <GalleryVerticalEnd className="size-4" />
+            <GalleryVerticalEnd className="w-4 h-4" />
           </div>
           core-crm
         </Link>
-        <LoginForm />
+        <Suspense fallback={null}>
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
-  )
+  );
 }
+
